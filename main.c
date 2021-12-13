348a7980cbe0bcdcab771c7921af7c929d887d86
SenshiPRC <pol.riubrogent@autonoma.cat>
2021-12-13 13:10:12 +0100

    Incluir

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
-void main(){
-    printf("Adeu");
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
-
-}
